df49bba18805977ea755766155547294c61b89a7
sayankhan313 <ksayan78619@gmail.com>
2026-01-10 02:00:24 +0000

    updated footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
-
 import HeroSection from "@/components/HeroSection";
 import ReferendumSection from "@/components/ReferendumSection";
 import Footer from "@/components/Footer";
 
 export default function HomePage() {
   return (
-    <div className="w-full">
-    
+    <div className="w-full" id="top">
       <main className="w-full">
         <HeroSection />
-        <ReferendumSection/>
-         
+        <ReferendumSection />
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
